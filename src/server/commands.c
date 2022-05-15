f1a07d84b4527db159b94a5ee9b5a20f010690d6
Cavonstavant <constant.vigneron@protonmail.com>
2022-05-15 22:04:08 +0200

    [*] Fix crash if ftp command template

--- src/server/commands.c
+++ src/server/commands.c
@@ -32,6 +32,8 @@ ftp_cmd_t get_ftp_cmd_template(char *ftp_cmd)
 {
     int i;
 
+    if (!ftp_cmd)
+        return (known_commands[14]);
     for (i = 0; known_commands[i].cmd; i++)
         if (strcmp(known_commands[i].cmd, ftp_cmd) == 0)
             return (known_commands[i]);
@@ -44,7 +46,7 @@ ftp_cmd_t *memset_ftp_cmd(ftp_cmd_t *ftp_cmd, char *cmd)
 
     if (cmd_template.cmd == 0){
         bzero(ftp_cmd, sizeof(ftp_cmd_t));
-        ftp_cmd->err_msg = strdup("502 Command not found");
+        ftp_cmd->err_msg = strdup("502 Command not found\r\n");
         ftp_cmd->reply_code = 502;
         return (ftp_cmd);
     }
